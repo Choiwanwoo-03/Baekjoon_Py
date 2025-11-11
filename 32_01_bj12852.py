71f82c7488ea298242d427666b5e5a6f8dc22108
wanu_c <chldhksdn37@gmail.com>
2025-11-11 20:32:08 +0900

    백준 32_01단계 문제 12852번

---
+++ 32_01_bj12852.py
@@ -0,0 +1,34 @@
+#   작성자 : 컴퓨터공학부 최완우
+#   작성일 : 2025.11.11
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+dp = [0] * (N + 1)
+path = [0] * (N + 1)
+
+for i in range(2, N + 1) :
+    dp[i] = dp[i - 1] + 1
+    path[i] = i - 1
+    
+    if i % 2 == 0 and dp[i] > dp[i // 2] + 1 :
+        dp[i] = dp[i // 2] + 1
+        path[i] = i // 2
+        
+    if i % 3 == 0 and dp[i] > dp[i // 3] + 1 :
+        dp[i] = dp[i // 3] + 1
+        path[i] = i // 3
+        
+print(dp[N])
+
+result = []
+current = N
+while current != 0 :
+    result.append(current)
+    if current == 1 :
+        break
+    current = path[current]
+    
+print(*result)
