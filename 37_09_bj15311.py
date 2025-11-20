eaa3f5cf6115185321322bb7b075bf7d28eb01e1
wanu_c <chldhksdn37@gmail.com>
2025-11-20 21:12:07 +0900

    feat : 백준 37_09단계 문제 15311번

---
+++ 37_09_bj15311.py
@@ -0,0 +1,16 @@
+#   작성자 : 컴퓨터공학부 최완우
+#   작성일 : 2025.11.20
+
+import sys
+input = sys.stdin.readline
+
+N = int(input().strip())
+
+L = 2000
+print(L)
+
+for _ in range(1000) :
+    sys.stdout.write("1 ")
+for _ in range(1000) :
+    sys.stdout.write("1000 ")
+sys.stdout.write("\n")
